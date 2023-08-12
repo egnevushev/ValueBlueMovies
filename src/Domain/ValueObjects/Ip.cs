e6030673cd2b642ac4d33649ffcd330e315b0bf9
egnevushev <e.gnevushev@gmail.com>
2023-08-12 18:00:20 +0200

    Fix ::1 IpAddress

--- src/Domain/ValueObjects/Ip.cs
+++ src/Domain/ValueObjects/Ip.cs
@@ -4,9 +4,21 @@ namespace Domain.ValueObjects;
 
 public sealed class Ip
 {
+    private const string Unknown = "unknown";
+    private const string Localhost = "127.0.0.1";
+    
     public string Value { get; private set; }
 
     private Ip(string value) => Value = value;
 
-    public static Ip Create(IPAddress? ipAddress) => new(ipAddress?.ToString() ?? "unknown");
+    public static Ip Create(IPAddress? ipAddress)
+    {
+        var ip = ipAddress?.ToString();
+        return ip switch
+        {
+            null => new Ip(Unknown),
+            "::1" => new Ip(Localhost),
+            _ => new Ip(ip)
+        };
+    }
 }
